cb766004de64be7ea08764ac6d11479350981ab9
antoinechalifour <antoine.chalifour@gmail.com>
2019-06-01 13:55:07 +0200

    feat: display current spots

--- src/components/SpotsMap/styles.ts
+++ src/components/SpotsMap/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components/macro";
+import { color, spacing } from "../../ui/Theme";
 
 export const Wrapper = styled.div`
   flex: 1;
@@ -8,6 +9,46 @@ export const Wrapper = styled.div`
   .map {
     position: absolute;
     width: 100%;
-    height: 100%;
+    height: 75%;
+  }
+`;
+
+export const SpotsList = styled.ul`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 25%;
+  display: flex;
+  overflow-x: auto;
+  padding: ${spacing("contentPadding")} 0;
+  scroll-snap-type: x mandatory;
+
+  > li {
+    flex-shrink: 0;
+    max-width: 80%;
+    scroll-snap-align: start;
+  }
+
+  > li + li {
+    margin-left: ${spacing("default")};
+  }
+`;
+
+// TODO: fix me with CSS pseudo elements?
+export const Pusher = styled.li`
+  flex-basis: 20%;
+`;
+
+export const SpotItem = styled.div`
+  padding: 20px;
+
+  > :first-child {
+    font-weight: bold;
+    font-size: 1.5rem;
+    margin-bottom: 10px;
+  }
+
+  > :nth-child(2) {
+    color: ${color("neutral60")};
   }
 `;
