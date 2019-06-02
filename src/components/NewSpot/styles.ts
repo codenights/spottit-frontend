5659b107ba12534d0da07fc0324b635ff061513e
antoinechalifour <antoine.chalifour@gmail.com>
2019-06-02 21:40:00 +0200

    create spot desktop view

--- src/components/NewSpot/styles.ts
+++ src/components/NewSpot/styles.ts
@@ -3,5 +3,7 @@ import styled from 'styled-components/macro'
 import { spacing } from '../../ui/Theme'
 
 export const Content = styled.div`
+  max-width: 600px;
+  margin: auto;
   padding: ${spacing('contentPadding')};
 `
